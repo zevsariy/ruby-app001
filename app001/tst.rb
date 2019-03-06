4e1d53bbd451a4e7ed92af8c674bf5b25c17bd00
zevsariy <zevsariy.app@gmail.com>
2019-03-06 20:58:43 +0300

    add array labs

--- app001/tst.rb
+++ app001/tst.rb
@@ -3,11 +3,24 @@ h = { "a" => 1000000000,
 "c" => 22222,
 "d" => 554353453453,
 :f => "dsfdsfsdfsd"}
-
+puts " "
 print h["d"]
-
+puts " "
 print h[:f]
 
 h.each { |x| puts x }
 
 h.each_with_index {|val, index| puts "#{val} => #{index}" }
+
+
+
+sergey = []
+
+h.each{|_index, val| sergey.push(val) }
+
+puts " "
+puts sergey
+puts " "
+puts sergey.join(" T_T ")
+spliter = sergey.join(" T_T ")
+puts spliter.split(" T_T ")
